587b6722084b75dbc1e30f67a37a7e47388f81cc
guikarist <guikarist@gmail.com>
2019-04-18 11:21:57 +0800

    Optimise codes of dqn_test

--- dqn_test.py
+++ dqn_test.py
@@ -1,12 +1,23 @@
+import pommerman
+
 from pommerman import agents
 from DQN.dqn_agent import DQNAgent
 from DQN.configs.q5_train_atari_nature import config
 from DQN.q1_schedule import LinearExploration, LinearSchedule
 
-import pommerman
+
+def main():
+    opponents = [
+        agents.SimpleAgent(),
+        agents.RandomAgent(),
+        agents.RandomAgent(),
+    ]
+
+    # train(opponents)
+    _test(opponents, 100, render=True)
 
 
-def train(opponents, train_from_scratch=False, render=False):
+def _train(opponents, train_from_scratch=False, render=False):
     env = pommerman.make('PommeFFACompetition-v0', [])
 
     # Exploration strategy
@@ -18,16 +29,12 @@ def train(opponents, train_from_scratch=False, render=False):
                                  config.lr_nsteps)
 
     # Initialize agents.
-    dqn_agent = DQNAgent(env, config, exp_schedule, lr_schedule, True, train_from_scratch=train_from_scratch)
-    agent_list = [dqn_agent] + opponents
-    dqn_agent_index = agent_list.index(dqn_agent)
-    for id_, agent in enumerate(agent_list):
-        agent.init_agent(id_, env.spec._kwargs['game_type'])  # TODO: Don't use protected member.
-    env.set_agents(agent_list)
+    dqn_agent, dqn_agent_index = _init_agents(env, exp_schedule, lr_schedule, opponents, train_from_scratch)
 
     t = 1
     while t < config.nsteps_train:
         state = env.reset()
+
         done = False
         while not done:
             t += 1
@@ -44,7 +51,7 @@ def train(opponents, train_from_scratch=False, render=False):
     env.close()
 
 
-def test(match_num=20, render=True):
+def _test(opponents, match_num=20, render=True):
     env = pommerman.make('PommeFFACompetition-v0', [])
 
     # Exploration strategy
@@ -55,45 +62,43 @@ def test(match_num=20, render=True):
                                  config.lr_nsteps)
 
     # Initialize agents.
-    dqn_agent = DQNAgent(env, config, exp_schedule, lr_schedule, False)
-    agent_list = [
-        dqn_agent,
-        agents.SimpleAgent(),
-        agents.RandomAgent(),
-        agents.RandomAgent(),
-    ]
-    dqn_agent_index = agent_list.index(dqn_agent)
-    for id_, agent in enumerate(agent_list):
-        agent.init_agent(id_, env.spec._kwargs['game_type'])  # TODO: Don't use protected member.
-    env.set_agents(agent_list)
+    dqn_agent, dqn_agent_index = _init_agents(env, exp_schedule, lr_schedule, opponents, False)
 
     count = 0
     win = 0
     for _ in range(match_num):
         state = env.reset()
+
         done = False
+
         while not done:
             if render:
                 env.render()
             actions = env.act(state)
             state, reward, done, info = env.step(actions)
-            if reward[dqn_agent_index] == 1:
+            if reward[0] == 1:
                 win += 1
                 print('win at episode %d' % count)
 
-            # if reward[dqn_agent_index] == -1 and not done:
-            #     # Stop the episode when the testing agent dies.
-            #     done = True
+            if reward[dqn_agent_index] == -1 and not done:
+                # Stop the episode when the testing agent dies.
+                done = True
         count += 1
     print(win / count)
 
     env.close()
 
 
+def _init_agents(env, exp_schedule, lr_schedule, opponents, train_from_scratch):
+    dqn_agent = DQNAgent(env, config, exp_schedule, lr_schedule, True, train_from_scratch=train_from_scratch)
+    agent_list = [dqn_agent] + opponents
+    index = agent_list.index(dqn_agent)
+    for id_, agent in enumerate(agent_list):
+        agent.init_agent(id_, env.spec._kwargs['game_type'])  # TODO: Don't use protected member.
+    env.set_agents(agent_list)
+
+    return dqn_agent, index
+
+
 if __name__ == '__main__':
-    train([
-        agents.SimpleAgent(),
-        agents.RandomAgent(),
-        agents.RandomAgent(),
-    ])
-    # test(100, render=True)
+    main()
